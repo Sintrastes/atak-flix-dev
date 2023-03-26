ee1dd5011fe10c06248503b0cdf348bdc1d98968
Nathan Bedell <nbedell@tulane.edu>
2023-03-25 19:16:10 -0600

    Add compile flix task.

--- app/src/main/java/com/atakmap/android/plugintemplate/PluginTemplateDropDownReceiver.java
+++ app/src/main/java/com/atakmap/android/plugintemplate/PluginTemplateDropDownReceiver.java
@@ -4,6 +4,7 @@ package com.atakmap.android.plugintemplate;
 import android.content.Context;
 import android.content.Intent;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.atak.plugins.impl.PluginLayoutInflater;
@@ -13,7 +14,6 @@ import com.atakmap.android.dropdown.DropDown.OnStateListener;
 import com.atakmap.android.dropdown.DropDownReceiver;
 
 import com.atakmap.coremap.log.Log;
-import com.myorg.SomeClass;
 
 import MyFlixApp.Ns;
 import dev.flix.runtime.Unit;
@@ -25,7 +25,7 @@ public class PluginTemplateDropDownReceiver extends DropDownReceiver implements
             .getSimpleName();
 
     public static final String SHOW_PLUGIN = "com.atakmap.android.plugintemplate.SHOW_PLUGIN";
-    private final View templateView;
+    private final LinearLayout templateView;
     private final Context pluginContext;
 
     /**************************** CONSTRUCTOR *****************************/
@@ -38,17 +38,13 @@ public class PluginTemplateDropDownReceiver extends DropDownReceiver implements
         // Remember to use the PluginLayoutInflator if you are actually inflating a custom view
         // In this case, using it is not necessary - but I am putting it here to remind
         // developers to look at this Inflator
-        templateView = PluginLayoutInflater.inflate(context,
+        templateView = (LinearLayout) PluginLayoutInflater.inflate(context,
                 R.layout.main_layout, null);
 
-        // Initialize flix Java dependencies
-        new SomeClass(
-                templateView.<TextView>findViewById(R.id.example_text)
+        // Initialize the Flix layout.
+        templateView.addView(
+                (TextView) Ns.m_textViewExample(context)
         );
-
-        // Initialize flix module
-        templateView.<TextView> findViewById(R.id.example_text)
-                .setText((String) Ns.m_showDomExample(new Unit()));
     }
 
     /**************************** PUBLIC METHODS *****************************/
